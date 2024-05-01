ac3073bedfdc986028a1d09ca15faf93ba95fe62
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-05-01 12:53:27 -0400

    Nuevo carrusel implementado

--- src/app/modules/home/pages/home-layout/home-layout.component.ts
+++ src/app/modules/home/pages/home-layout/home-layout.component.ts
@@ -25,7 +25,7 @@ export class HomeLayoutComponent {
   /**
    * Productos
    */
-  productItems: any[] = [
+  productItems = [
     {
       image: carouselImages.perfume1
     },
@@ -42,108 +42,5 @@ export class HomeLayoutComponent {
       image: carouselImages.perfume5
     },
   ]
-
-  /**
-   * Producto seleccionado
-   */
-  productSelected: any
-
-  @HostListener('window:resize', ['$event'])
-  onResize(e: Event) {
-
-    //* Veo si la pantalla es lo suficientemente pequeña
-    if (window.innerWidth <= 992) {
-      this.changeListMode()
-    } else {
-      this.productItems = []
-    }
-
-  }
-
-  constructor() {
-
-  }
-
-  ngOnInit() {
-
-    //* Veo si la pantalla es lo suficientemente pequeña
-    if (window.innerWidth <= 992) {
-      this.changeListMode()
-    } else {
-      this.productItems = []
-    }
-
-  }
-
-  /**
-   * Funcion para seleccionar el siguiente elemento del carrucel
-   */
-  nextElement() {
-    // selecciono el ultimo elemento de mi arreglo de imagenes
-    const last: number = this.productItems.length - 1;
-
-    // selecciono la imagen que corresponde
-    const nextImg = ++this.productSelected;
-    if (nextImg > last) {
-      this.selectTemplate(0);
-    }
-    else {
-      this.selectTemplate(nextImg);
-    }
-  }
-
-  /**
-   * Funcion para seleccionar el anterior elemento del carrucel
-   */
-  prevElement() {
-    // selecciono el ultimo elemento de mi arreglo de imagenes
-    const last: number = this.productItems.length - 1;
-
-    // selecciono la imagen que corresponde
-    const nextImg = this.productSelected - 1;
-    if (nextImg < 0) {
-      this.selectTemplate(last);
-    }
-    else {
-      this.selectTemplate(nextImg);
-    }
-  }
-
-  /**
-   * Funcion que selescciona el Templateo que se muestra en detalle segun la imagen que se seleccione
-   * @param idImg
-   */
-  selectTemplate(idImg: number) {
-
-    // Guardo el id de la imagen seleccionada
-    this.productSelected = idImg;
-
-  }
-
-  /**
-   * Genera una lista con todos los items del listado con secciones de items
-   */
-  changeListMode() {
-
-    //* Vacío el listado
-    this.productItems = [
-      {
-        image: carouselImages.perfume1
-      },
-      {
-        image: carouselImages.perfume2
-      },
-      {
-        image: carouselImages.perfume3
-      },
-      {
-        image: carouselImages.perfume4
-      },
-      {
-        image: carouselImages.perfume5
-      },
-    ]
-
-  }
 
 }
