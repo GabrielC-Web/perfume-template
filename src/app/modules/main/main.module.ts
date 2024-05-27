6558ad7ce6d1d121319926c57ed4764e39686c32
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-05-27 19:50:29 -0400

    incluido breadcrumb

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -3,9 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
 import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
-import { CmmModule } from 'src/app/common/common.module';
 import { MainHeaderComponent } from './components/main-header/main-header.component';
 import { MainFooterComponent } from './components/main-footer/main-footer.component';
+import { CmmModule } from 'src/app/common/common.module';
 
 @NgModule({
   declarations: [
